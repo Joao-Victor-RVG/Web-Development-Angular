9eaf9e7751d5a5ac337172ef0b1f5ecd17bd3da4
João Victor Rocha Vilela Godoi <80182535+Joao-Victor-RVG@users.noreply.github.com>
2023-02-02 21:53:47 -0300

    Arrow Function

---
+++ JavaScript/2- Arrow Function/Example/script.js
@@ -0,0 +1,39 @@
+// let multiplicador = function() {
+
+//     return 5 * 5
+// }
+
+
+// console.log(multiplicador)
+// console.log(multiplicador())
+
+
+// let multiplicador = (n) => {
+//     return n* n
+// }
+
+// console.log(multiplicador(7))
+
+
+
+// let multiplicador = (n) => {
+//     return n* n
+// }
+
+// console.log(multiplicador(8))
+
+
+// let multiplicador = n => n * n
+// console.log(multiplicador(5, 4))
+
+// let oddOrEven = n => {
+//     if(n % 2 === 0) {
+//         return 'Even'
+//     } else {
+//         return 'Odd'
+//     }
+// }
+
+// let oddOrEven = n => n % 2 === 0 ? 'Odd' : 'Even'
+// console.log(oddOrEven(5))
+
